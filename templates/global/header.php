e5758d60cb7c6797960dd1fb4f49b233e01bb723
carlos (minimal) <padi_05@yahoo.com>
2018-02-07 02:29:28 -0500

    Logo styles

--- templates/global/header.php
+++ templates/global/header.php
@@ -4,7 +4,7 @@
 		<?php _t('top-nav'); ?>
 	</section>
 	<section id="logo">
-		<h1><a class="image-link" href="/home.html"><img src="/img/logo.png" alt="Front Porch Studio"></a></h1>
+		<h1><a class="image-link" href="/home.html"><img src="/img/logo.svg" alt="Front Porch Studio"></a></h1>
 	</section>
 	<?php _t('main-nav'); ?>
 </header>
