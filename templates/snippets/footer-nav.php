453c5fe75470b03dbd6b826eddcc650a66e4eadb
carlos (minimal) <padi_05@yahoo.com>
2018-02-07 22:16:36 -0500

    Footer nav styles

---
+++ templates/snippets/footer-nav.php
@@ -0,0 +1,21 @@
+<nav>
+	<h2>About</h2>
+	<ul>
+		<li><a href="/about.html">My Story</a></li>
+	</ul>
+	<h2>Shop</h2>
+	<ul>
+		<li><a href="/collection.html">Wholesale</a></li>
+		<li><a href="/collection.html">Gift Cards</a></li>
+	</ul>
+	<h2>Contact</h2>
+	<ul>
+		<li><a href="mailto:info@fpsnantucket.com">info@fpsnantucket.com</a></li>
+		<li><a href="tel:123-456-7980">123-456-7980</a></li>
+	</ul>
+	<h2>Help</h2>
+	<ul>
+		<li><a href="/faq.html">F.A.Q.</a></li>
+		<li><a href="/page.html">Policies</a></li>
+	</ul>
+</nav>
