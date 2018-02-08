1d0097f9d11668e6a7cd5481adc85c087ea56fc2
Carlos Padilla <padi_05@yahoo.com>
2018-02-08 17:17:03 -0500

    Footer social initial

--- templates/global/footer.php
+++ templates/global/footer.php
@@ -4,9 +4,7 @@
 	<div id="footer-cols">
 		<?php _t('contact'); ?>
 		<?php _t('footer-nav'); ?>
-		<section id="social">
-			<?php _t('social'); ?>
-		</section>
+		<?php _t('social'); ?>
 	</div>
 	<p id="fine-print">&copy; Copyright 2017 Front Porch Studio Nantucket. All rights reserved. <a href="http://sheilabuchanandesigns.com" target="_blank">Site design</a></p>
 </footer>
