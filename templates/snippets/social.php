0b11da470248188150895440bec3955ce9e10a31
carlos (minimal) <padi_05@yahoo.com>
2018-02-07 23:03:16 -0500

    Footer columns layout

--- templates/snippets/social.php
+++ templates/snippets/social.php
@@ -1,5 +1,14 @@
 <section>
 	<header>
+		<h2>Follow Me <a href="https://instagram.com" target="_blank" rel="nofollow">@fpsnantucket</a></h2>
 	</header>
+	<main>
+		<ul>
+			<li><img src="/img/follow/a.jpg" alt=""></li>
+			<li><img src="/img/follow/b.jpg" alt=""></li>
+			<li><img src="/img/follow/b.jpg" alt=""></li>
+			<li><img src="/img/follow/a.jpg" alt=""></li>
+		</ul>
+	</main>
 </section>
 		
