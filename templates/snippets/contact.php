2a03aa856851bf3d816af4828d6573a0b835e4d9
carlos (minimal) <padi_05@yahoo.com>
2018-02-07 22:05:04 -0500

    Contact section

---
+++ templates/snippets/contact.php
@@ -0,0 +1,23 @@
+<article>
+	<header>
+		<h2>Visit The Shop!</h2>
+	</header>
+	<main>
+		<p>
+			5 Washington Street<br>
+			Nantucket, MA 02554<br>
+			(123) 456-7980
+		</p>
+		<p>
+			10:00am - 5:00pm<br>
+			Everyday
+		</p>
+	</main>
+	<footer>
+		<ul>
+			<li><a class="image-link" href="https://facebook.com" target="_blank" rel="nofollow"><img src="/img/icons/facebook.svg" alt="Like Us On Facebook"></a></li>
+			<li><a class="image-link" href="https://twitter.com" target="_blank" rel="nofollow"><img src="/img/icons/twitter.svg" alt="Follow Us On Twitter"></a></li>
+			<li><a class="image-link" href="https://instagram.com" target="_blank" rel="nofollow"><img src="/img/icons/instagram.svg" alt="Follow Us On Instagram"></a></li>
+		</ul>
+	</footer>
+</article>
