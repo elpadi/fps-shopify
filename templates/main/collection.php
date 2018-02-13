e6dfbcc8a4e55287a8c3be99b3e90b53375d9424
carlos (minimal) <padi_05@yahoo.com>
2018-02-13 00:01:45 -0500

    Collection template html

--- templates/main/collection.php
+++ templates/main/collection.php
@@ -1,17 +1,38 @@
 <main id="collection-main" class="template-main single-column">
 	<header>
-		<img src="/img/collection.jpg" alt="Collection Background">
-		<h1>Collection Title</h1>
+		<img src="/img/shopify/Watercolors-Banner_2048x2048.jpg" alt="Collection Background">
+		<p>A current mini gallery of my available watercolors. All my work comes framed in bright crisp white handmade Alderwood frames from Rustic &amp; Refined and can be stood on a shelf with a peg (included), or wall hung. My work can also be scene in my Shop, Front Porch Studio and at the Saturday Farmers and Artisian Market on Nantucket Island. Sign up for my newsletter to get my Summer 2017 Market dates.</p>
 	</header>
 	<main>
 		<h2>Sub Heading</h2>
 		<ul class="product-grid">
 			<li>
-				<article>
-					<a class="image-link" href="/product.php"><img src="/img/product.jpg" alt="Product Image"></a>
-					<h3>Product Title</h3>
-					<h4>$36.00</h4>
-				</article>
+				<a class="image-link" href="/product.php">
+					<img src="/img/shopify/IMG_7107_grande.jpg" alt="Product Image">
+				</a>
+				<h3><a href="/product.php">Watercolor of Nantucket Island</a></h3>
+				<h4>$36.00</h4>
+			</li>
+			<li>
+				<a class="image-link" href="/product.php">
+					<img src="/img/shopify/IMG_7135_grande.jpg" alt="Product Image">
+				</a>
+				<h3><a href="/product.php">Blueberry Basket Watercolor</a></h3>
+				<h4>$36.00</h4>
+			</li>
+			<li>
+				<a class="image-link" href="/product.php">
+					<img src="/img/shopify/IMG_0581_grande.jpg" alt="Product Image">
+				</a>
+				<h3><a href="/product.php">4&quo;&quo; x 4&quo;&quo; &ldquo;Hello Friend&rdquo; Watercolor</a></h3>
+				<h4>$36.00</h4>
+			</li>
+			<li>
+				<a class="image-link" href="/product.php">
+					<img src="/img/shopify/IMG_0225_grande.jpg" alt="Product Image">
+				</a>
+				<h3><a href="/product.php">4&quo;&quo; x 4&quo;&quo; Watercolor &ldquo;Sweet Isle Of Mine&rdquo;</a></h3>
+				<h4>$36.00</h4>
 			</li>
 		</ul>
 	</main>
